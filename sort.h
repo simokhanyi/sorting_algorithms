38627b5d6e9d53041cd270e5d235773c1b5b9a8c
TOMKIBOI <tomkiboi@yahoo.com>
2023-11-20 15:56:35 -0800

    quick sort func

--- sort.h
+++ sort.h
@@ -32,5 +32,8 @@ void merge_sort(int *array, size_t size);
 void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
+void swap(int *a, int *b);
+int lomuto_partition(int *array, int low, int high, size_t size);
+void quicksort(int *array, int low, int high, size_t size);
 
 #endif

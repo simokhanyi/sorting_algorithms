f8231aabff741a261bccf8856ac338f80fde52bd
TOMKIBOI <tomkiboi@yahoo.com>
2023-11-21 14:06:53 -0800

    betty

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -27,7 +27,7 @@ void shell_sort(int *array, size_t size)
  * @n: intervale
  * Return: nothing to return
  */
-  void knut_shell_sort(int *a, int size, int n)
+void knut_shell_sort(int *a, int size, int n)
 {
 	int tempo, i, j;
 
